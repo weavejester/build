1a1ba4a7d356843fe80b946aec16518cf0ca15c4
James Reeves <jreeves@weavejester.com>
2021-10-30 15:50:46 +0100

    Add pom task to build

--- build.clj
+++ build.clj
@@ -1,6 +1,18 @@
 (ns build
-  (:require [clojure.tools.build.api :as b]
-            [org.corfield.build :as bb]))
+  (:require [clojure.string :as str]
+            [clojure.tools.build.api :as b]
+            [org.corfield.build :as bb]
+            [tools-pom.tasks :as pom]))
 
-(defn hello [_]
-  (println "Hello World"))
+(defn- git [& args]
+  (b/process {:command-args (into ["git"] args)}))
+
+(defn git-head-tag []
+  (some-> (git "describe" "--exact-match" "--abbrev=0") :out str/trim))
+
+(defn pom [_]
+  (pom/pom {:lib 'weavejester/build
+            :version (git-head-tag)
+            :write-pom true
+            :validate-pom true
+            :pom {:description "testing"}}))
