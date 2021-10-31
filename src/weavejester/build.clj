dbb19fea302e469ac932a3b67d54dd61f116aff2
James Reeves <jreeves@weavejester.com>
2021-10-31 21:05:24 +0000

    Add default project values

--- src/weavejester/build.clj
+++ src/weavejester/build.clj
@@ -1,23 +1,40 @@
 (ns weavejester.build
   (:require [clojure.string :as str]
-            [clojure.tools.build.api :as b]))
-
-(defn- git [& args]
-  (b/process {:command-args (into ["git"] args)}))
-
-(defn- git-head-tag []
-  (some-> (git "describe" "--exact-match" "--abbrev=0") :out str/trim))
+            [clojure.tools.build.api :as b]
+            [clojure.java.io :as io]))
 
 (defn- read-project []
   (read-string (slurp "project.edn")))
 
 (def ^:private default-project
-  {:class-dir  "target/classes"
-   :src-dirs   ["src" "resources"]
+  {:src-dirs   ["src" "resources"]
    :target-dir "target"})
 
+(defn- git [& args]
+  (b/process {:command-args (into ["git"] args)
+              :out :capture
+              :err :ignore}))
+
+(defn- default-version []
+  (some-> (git "describe" "--exact-match" "--abbrev=0") :out str/trim))
+
+(defn- default-class-dir [{:keys [target-dir]}]
+  (str (io/file target-dir "classes")))
+
+(defn- default-jar-file [{:keys [lib target-dir version]}]
+  (str (io/file target-dir (str lib "-" version ".jar"))))
+
+(defn- update-derived-defaults
+  [{:keys [class-dir jar-file target-dir version] :as project}]
+  (cond-> project
+    (nil? version)   (assoc :version   (default-version))
+    (nil? class-dir) (as-> p (assoc p :class-dir (default-class-dir p)))
+    (nil? jar-file)  (as-> p (assoc p :jar-file  (default-jar-file  p)))))
+
 (def ^:private project
-  (delay (merge default-project (read-project))))
+  (delay (->> (read-project)
+              (merge default-project)
+              (update-derived-defaults))))
 
 (defn jar [_]
   (doto (assoc @project :basis (b/create-basis))
