4ed9225b50a25dab0147c1ea74f98cb5da2a408a
James Reeves <jreeves@weavejester.com>
2021-10-31 01:30:42 +0000

    Move pom function into library

--- build.clj
+++ build.clj
@@ -1,11 +1,7 @@
 (ns build
-  (:require [org.corfield.build :as bb]
-            [tools-pom.tasks :as pom]
-            [weavejester.build :as wb]))
+  (:require [weavejester.build :as wb]))
 
 (defn pom [_]
-  (pom/pom {:lib 'weavejester/build
-            :version (wb/git-head-tag)
-            :write-pom true
-            :validate-pom true
-            :pom {:description "testing"}}))
+  (wb/pom {:lib 'weavejester/build
+           :version (wb/git-head-tag)
+           :description "testing"}))
--- src/weavejester/build.clj
+++ src/weavejester/build.clj
@@ -1,9 +1,17 @@
 (ns weavejester.build
   (:require [clojure.string :as str]
-            [clojure.tools.build.api :as b]))
+            [clojure.tools.build.api :as b]
+            [tools-pom.tasks :as pom]))
 
 (defn- git [& args]
   (b/process {:command-args (into ["git"] args)}))
 
 (defn git-head-tag []
   (some-> (git "describe" "--exact-match" "--abbrev=0") :out str/trim))
+
+(defn pom [{:keys [lib version] :as opts}]
+  (pom/pom {:lib          lib
+            :version      version
+            :pom          (dissoc opts :lib :version)
+            :write-pom    true
+            :validate-pom true}))
