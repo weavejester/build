dcc241c32a1c3790e988d749cad602feed9592d3
James Reeves <jreeves@weavejester.com>
2021-10-31 16:32:39 +0000

    Move all build tasks into library

--- build.clj
+++
@@ -1,7 +0,0 @@
-(ns build
-  (:require [weavejester.build :as wb]))
-
-(defn pom [_]
-  (wb/pom {:lib 'weavejester/build
-           :version (wb/git-head-tag)
-           :description "testing"}))
--- src/weavejester/build.clj
+++ src/weavejester/build.clj
@@ -6,12 +6,19 @@
 (defn- git [& args]
   (b/process {:command-args (into ["git"] args)}))
 
-(defn git-head-tag []
+(defn- git-head-tag []
   (some-> (git "describe" "--exact-match" "--abbrev=0") :out str/trim))
 
-(defn pom [{:keys [lib version] :as opts}]
-  (pom/pom {:lib          lib
-            :version      version
-            :pom          (dissoc opts :lib :version)
-            :write-pom    true
-            :validate-pom true}))
+(defn- read-project []
+  (read-string (slurp "project.edn")))
+
+(def ^:private project
+  (delay (read-project)))
+
+(defn pom [_]
+  (let [{:keys [lib version] :as opts} @project]
+    (pom/pom {:lib          lib
+              :version      version
+              :pom          (dissoc opts :lib :version)
+              :write-pom    true
+              :validate-pom true})))
