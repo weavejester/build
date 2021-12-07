2d47d612d5687b6e8f261e90c5b71b5eab744fcf
James Reeves <jreeves@weavejester.com>
2021-12-07 06:16:52 +0000

    Add uberjar command

--- src/weavejester/build.clj
+++ src/weavejester/build.clj
@@ -18,13 +18,17 @@
 (defn- default-jar-file [{:keys [lib target-dir version]}]
   (str (io/file target-dir (str lib "-" version ".jar"))))
 
+(defn- default-uberjar-file [{:keys [lib target-dir version]}]
+  (str (io/file target-dir (str lib "-" version "-uber.jar"))))
+
 (defn- update-derived-defaults
-  [{:keys [class-dir jar-file target-dir version scm] :as project}]
+  [{:keys [class-dir jar-file uber-file target-dir version scm] :as project}]
   (cond-> project
     (nil? version)   (assoc :version (git/default-version))
     (nil? scm)       (assoc :scm     (git/github-scm-map))
     (nil? class-dir) (as-> p (assoc p :class-dir (default-class-dir p)))
-    (nil? jar-file)  (as-> p (assoc p :jar-file  (default-jar-file  p)))))
+    (nil? jar-file)  (as-> p (assoc p :jar-file  (default-jar-file  p)))
+    (nil? uber-file) (as-> p (assoc p :uber-file (default-uberjar-file p)))))
 
 (def ^:private project
   (delay (->> (read-project)
@@ -36,3 +40,10 @@
     (pom/write-pom)
     (b/copy-dir)
     (b/jar)))
+
+(defn uberjar [_]
+  (doto (assoc @project :basis (b/create-basis))
+    (pom/write-pom)
+    (b/copy-dir)
+    (b/compile-clj)
+    (b/uber)))
