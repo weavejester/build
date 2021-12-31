e7262a6a91c93a291fdfda0b60ea4fef69f418e0
James Reeves <jreeves@weavejester.com>
2021-12-31 17:59:07 +0000

    Factor out project reading

---
+++ src/weavejester/build/project.clj
@@ -0,0 +1,33 @@
+(ns weavejester.build.project
+  (:require [clojure.java.io :as io]
+            [weavejester.build.git :as git]))
+
+(defn- read-project []
+  (read-string (slurp "project.edn")))
+
+(def ^:private default-project
+  {:src-dirs   ["src" "resources"]
+   :target-dir "target"})
+
+(defn- default-class-dir [{:keys [target-dir]}]
+  (str (io/file target-dir "classes")))
+
+(defn- default-jar-file [{:keys [lib target-dir version]}]
+  (str (io/file target-dir (str lib "-" version ".jar"))))
+
+(defn- default-uberjar-file [{:keys [lib target-dir version]}]
+  (str (io/file target-dir (str lib "-" version "-uber.jar"))))
+
+(defn- update-derived-defaults
+  [{:keys [class-dir jar-file uber-file version scm] :as project}]
+  (cond-> project
+    (nil? version)   (assoc :version (git/default-version))
+    (nil? scm)       (assoc :scm     (git/github-scm-map))
+    (nil? class-dir) (as-> p (assoc p :class-dir (default-class-dir p)))
+    (nil? jar-file)  (as-> p (assoc p :jar-file  (default-jar-file  p)))
+    (nil? uber-file) (as-> p (assoc p :uber-file (default-uberjar-file p)))))
+
+(def project
+  (delay (->> (read-project)
+              (merge default-project)
+              (update-derived-defaults))))
--- src/weavejester/build/tool.clj
+++ src/weavejester/build/tool.clj
@@ -1,47 +1,16 @@
 (ns weavejester.build.tool
-  (:require [clojure.java.io :as io]
-            [clojure.tools.build.api :as b]
-            [weavejester.build.git :as git]
+  (:require [clojure.tools.build.api :as b]
+            [weavejester.build.project :as p]
             [weavejester.build.write-pom :as pom]))
 
-(defn- read-project []
-  (read-string (slurp "project.edn")))
-
-(def ^:private default-project
-  {:src-dirs   ["src" "resources"]
-   :target-dir "target"})
-
-(defn- default-class-dir [{:keys [target-dir]}]
-  (str (io/file target-dir "classes")))
-
-(defn- default-jar-file [{:keys [lib target-dir version]}]
-  (str (io/file target-dir (str lib "-" version ".jar"))))
-
-(defn- default-uberjar-file [{:keys [lib target-dir version]}]
-  (str (io/file target-dir (str lib "-" version "-uber.jar"))))
-
-(defn- update-derived-defaults
-  [{:keys [class-dir jar-file uber-file version scm] :as project}]
-  (cond-> project
-    (nil? version)   (assoc :version (git/default-version))
-    (nil? scm)       (assoc :scm     (git/github-scm-map))
-    (nil? class-dir) (as-> p (assoc p :class-dir (default-class-dir p)))
-    (nil? jar-file)  (as-> p (assoc p :jar-file  (default-jar-file  p)))
-    (nil? uber-file) (as-> p (assoc p :uber-file (default-uberjar-file p)))))
-
-(def ^:private project
-  (delay (->> (read-project)
-              (merge default-project)
-              (update-derived-defaults))))
-
 (defn jar [_]
-  (doto (assoc @project :basis (b/create-basis))
+  (doto (assoc @p/project :basis (b/create-basis))
     (pom/write-pom)
     (b/copy-dir)
     (b/jar)))
 
 (defn uberjar [_]
-  (doto (assoc @project :basis (b/create-basis))
+  (doto (assoc @p/project :basis (b/create-basis))
     (pom/write-pom)
     (b/copy-dir)
     (b/compile-clj)
