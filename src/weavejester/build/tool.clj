b829c913352e828c5f8f0ad980904a25c39cd86e
James Reeves <jreeves@weavejester.com>
2022-01-01 13:25:33 +0000

    Add basic init tool for copying bb.edn

--- src/weavejester/build/tool.clj
+++ src/weavejester/build/tool.clj
@@ -1,7 +1,15 @@
 (ns weavejester.build.tool
   (:require [clojure.tools.build.api :as b]
             [weavejester.build.project :as p]
-            [weavejester.build.write-pom :as pom]))
+            [weavejester.build.write-pom :as pom]
+            [clojure.java.io :as io]))
+
+(defn init [_]
+  (let [src  (io/resource "weavejester/build/bb.edn")
+        dest (io/file "bb.edn")]
+    (when-not (.exists dest)
+      (with-open [in (io/input-stream src)]
+        (io/copy in dest)))))
 
 (defn jar [_]
   (doto (assoc @p/project :basis (b/create-basis))
