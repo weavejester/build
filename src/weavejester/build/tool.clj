71ffeb4632e4e469a43f4b3300c2616dc597fb7f
James Reeves <jreeves@weavejester.com>
2022-01-01 16:45:15 +0000

    Add correct dep version to generated bb.edn

--- src/weavejester/build/tool.clj
+++ src/weavejester/build/tool.clj
@@ -1,15 +1,32 @@
 (ns weavejester.build.tool
-  (:require [clojure.tools.build.api :as b]
+  (:require [clojure.edn :as edn]
+            [clojure.java.io :as io]
+            [clojure.string :as str]
+            [clojure.tools.build.api :as b]
             [weavejester.build.project :as p]
-            [weavejester.build.write-pom :as pom]
-            [clojure.java.io :as io]))
+            [weavejester.build.write-pom :as pom]))
+
+(defn- read-deps []
+  (edn/read-string (slurp (io/file b/*project-root* "deps.edn"))))
+
+(defn- find-self-version []
+  (-> (read-deps) :aliases :build :deps
+      (get 'dev.weavejester/build {:local/root "."})))
+
+(defn- replace-bbedn-version [s version]
+  (str/replace s "VERSION" (pr-str version)))
+
+(defn- write-bbedn []
+  (if (.exists (io/file b/*project-root* "bb.edn"))
+    (println "Skipping bb.edn as it already exists")
+    (let [bbedn (-> (io/resource "weavejester/build/bb.edn.tmpl")
+                    (slurp)
+                    (replace-bbedn-version (find-self-version)))]
+      (b/write-file {:path "bb.edn" :string bbedn})
+      (println "Written bb.edn"))))
 
 (defn init [_]
-  (let [src  (io/resource "weavejester/build/bb.edn")
-        dest (io/file "bb.edn")]
-    (when-not (.exists dest)
-      (with-open [in (io/input-stream src)]
-        (io/copy in dest)))))
+  (write-bbedn))
 
 (defn jar [_]
   (doto (assoc @p/project :basis (b/create-basis))
